74abae52d0e9d682ea4cd576a7c2823e7bdb3b58
igor.kostromin <elwood.su@gmail.com>
2014-03-24 21:56:55 +0400

    + Added ICommand and ICommandSource interfaces.

---
+++ ConsoleFramework/Events/ICommand.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleFramework.Events
+{
+    ///<summary>
+    /// An interface that allows an application author to define a method to be invoked.
+    ///</summary>
+    public interface ICommand
+    {
+        /// <summary>
+        /// Raised when the ability of the command to execute has changed.
+        /// </summary>
+        event EventHandler CanExecuteChanged;
+
+        /// <summary>
+        /// Returns whether the command can be executed.
+        /// </summary>
+        /// <param name="parameter">A parameter that may be used in executing the command. This parameter may be ignored by some implementations.</param>
+        /// <returns>true if the command can be executed with the given parameter and current state. false otherwise.</returns>
+        bool CanExecute(object parameter);
+
+        /// <summary>
+        /// Defines the method that should be executed when the command is executed.
+        /// </summary>
+        /// <param name="parameter">A parameter that may be used in executing the command. This parameter may be ignored by some implementations.</param>
+        void Execute(object parameter);
+    }
+}
---
+++ ConsoleFramework/Events/ICommandSource.cs
@@ -0,0 +1,35 @@
+﻿using ConsoleFramework.Controls;
+
+namespace ConsoleFramework.Events
+{
+    ///<summary>
+    /// An interface for classes that know how to invoke a Command.
+    ///</summary>
+    public interface ICommandSource
+    {
+        /// <summary>
+        /// The command that will be executed when the class is "invoked."
+        /// Classes that implement this interface should enable or disable based on the command's CanExecute return value.
+        /// The property may be implemented as read-write if desired.
+        /// </summary>
+        ICommand Command {
+            get;
+        }
+
+        /// <summary>
+        /// The parameter that will be passed to the command when executing the command.
+        /// The property may be implemented as read-write if desired.
+        /// </summary>
+        object CommandParameter {
+            get;
+        }
+
+        /// <summary>
+        /// An element that an implementor may wish to target as the destination for the command.
+        /// The property may be implemented as read-write if desired.
+        /// </summary>
+        Control CommandTarget {
+            get;
+        }
+    }
+}
