65173c912d917447075bffce80f1987e2e170021
Friedrich von Never <friedrich@fornever.me>
2017-09-03 11:53:15 +0700

    #33: remove GuidAttribute from AssemblyInfo
    This attribute is not available in .NET Standard. And we don't want to
    expose our libraries to COM anyway.

--- Examples/Properties/AssemblyInfo.cs
+++ Examples/Properties/AssemblyInfo.cs
@@ -19,9 +19,6 @@ using System.Runtime.InteropServices;
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("b8c5d50c-f075-43b2-9474-63369ba504be")]
-
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
--- TestProject1/Properties/AssemblyInfo.cs
+++ TestProject1/Properties/AssemblyInfo.cs
@@ -19,9 +19,6 @@ using System.Runtime.InteropServices;
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("5f89259b-1bd5-4402-84e4-7abab1fc7450")]
-
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
